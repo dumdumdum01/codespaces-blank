a7cb241c38219777e3cd1f30b4d691e7c3214b26
Marwin pineda <133139361+dumdumdum01@users.noreply.github.com>
2023-06-03 13:54:12 +0000

    clean

--- ngrok.sh
+++ ngrok.sh
@@ -1,2 +1,2 @@
-./ngrok config add-authtoken 2QhH15LmW1h5SgLhNZdr9KU1BIB_3Hcee1SHSVLaNYoffjEya
-./ngrok tcp 25565
+ngrok config add-authtoken 2QhH15LmW1h5SgLhNZdr9KU1BIB_3Hcee1SHSVLaNYoffjEya
+ngrok tcp 25565
